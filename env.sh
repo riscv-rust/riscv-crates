8e91d4770d8133958af702b303c25401317c39db
David Craven <david@craven.ch>
2018-02-23 09:19:35 +0100

    Initial commit.

---
+++ env.sh
@@ -0,0 +1,6 @@
+export RISCV_RUST_TOOLCHAIN=~/repos/riscv-rust
+export TOOLCHAIN=$RISCV_RUST_TOOLCHAIN/toolchain
+
+export PATH=/opt/rust/bin:/opt/riscv/bin:~/.cargo/bin:$PATH
+export XARGO_RUST_SRC=~/repos/rust/src
+export RUST_TARGET_PATH=$PWD
