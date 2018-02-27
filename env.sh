959fecf2fc0117d4965dfc4a5d8a82bbf95bacb8
David Craven <david@craven.ch>
2018-02-27 08:49:54 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,6 +1,5 @@
-export RISCV_RUST_TOOLCHAIN=~/repos/riscv-rust
-export TOOLCHAIN=$RISCV_RUST_TOOLCHAIN/toolchain
-
 export PATH=/opt/rust/bin:/opt/riscv/bin:~/.cargo/bin:$PATH
 export XARGO_RUST_SRC=~/repos/rust/src
 export RUST_TARGET_PATH=$PWD
+export TOOLCHAIN=/opt/rust
+export LD_LIBRARY_PATH=/opt/riscv/lib
